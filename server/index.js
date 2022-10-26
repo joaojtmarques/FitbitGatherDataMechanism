443b5a17645364e62a772cd55b10925bc8043879
joaojtmarques <joaojtmarques@LAPTOP-K82SV5GU.localdomain>
2022-10-26 16:32:10 +0100

    Cleaned code

--- server/index.js
+++ server/index.js
@@ -36,110 +36,16 @@ var server = https.createServer(options, function (r, s) {
     
             console.log(body + "\n");
 	    logger.write(body + "\n");
-        /*   
-	 for (var el in obj) {
-              console.log(el + " : " + obj[el])
-              logger.write(el + " : " + obj[el])
-            }
-            console.log("\n")
-         */
+        
 	 }
         }
         
         s.write('OK'); 
         s.end(); 
     });
-    /*
-    console.log("received a request!");
-    console.log(req.headers)
-    //console.log(req);
-    var bmpString = "";
-    res.on('data', function(data) { 
-      console.log("data after")
-      console.log(data)
-      bmpString += data; });
-    res.on('close', function() { 
-      console.log(bmpString);
-      // ...
-    });
-    */
-
     
   }
-  //res.writeHead(200);
-  //res.end("ended");
   
 })
 
 server.listen(9000, () => console.log("My server is listening on port 9000"));
-
-
-/*
-
-const wss = new WebSocket.Server({ server });
-wss.on('connection', function connection(ws) {
-  console.log("server up!");
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
-
-  ws.send('something');
-  console.log("sent something")
-});
-
-*/
-
-/*
-
-const websocket = new WebSocket.Server({ server });
-server.listen(8080, () => console.log("My server is listening on port 8080"));
-
-
-//when a legit websocket request comes listen to it and get the connection .. once you get a connection thats it! 
-websocket.on("request", request=> {
-  console.log("received request");
-  connection = request.accept(null, request.origin)
-  connection.on("open", () => console.log("Opened!!!"))
-  connection.on("close", () => console.log("CLOSED!!!"))
-  connection.on("message", message => {
-
-      console.log(`Received message ${message.utf8Data}`)
-      connection.send(`got your message: ${message.utf8Data}`)
-  })
-
-
-  //use connection.send to send stuff to the client 
-  sendevery5seconds();
-  
-
-})
-
-function sendevery5seconds(){
-
-  connection.send(`Message ${Math.random()}`);
-
-  setTimeout(sendevery5seconds, 5000);
-
-
-}
-
-
-/*
-const WebSocket = require('ws')
- const server = new WebSocket.Server({ port: '8080' })
-
-server.on('connection', socket => { 
-  console.log("server up!");
-
-  socket.on('message', message => {
-
-    socket.send(`Roger that! ${message}`);
-    console.log("message ", message);
-
-   });
-
- });
-*/
-
-
- 
